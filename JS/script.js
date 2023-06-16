09d0d042897edeabbed896691368d287ac01d71c
Marcos <mark7elias@gmail.com>
2023-06-15 21:22:25 -0700

    changed font, setting up general structure

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+let orderButton = document.querySelector("#order-button");
+
+let paragraph = document.querySelector("#practice");
+
+orderButton.onclick = function(){
+    window.alert("button clicked");
+
+    paragraph.textContent = "hello world, how are you doing";
+
+
+
+}
+
+
