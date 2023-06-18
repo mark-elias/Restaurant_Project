6ef4a41d604ff2fec307df28bf6d6095fe47fc9d
Marcos <mark7elias@gmail.com>
2023-06-17 19:06:52 -0700

    fixed up the Footer section and the Nav Bar

--- JS/dark_mode.js
+++ JS/dark_mode.js
@@ -3,6 +3,8 @@ const darkMode_button = document.getElementById("dark-mode-button");
 const navbar = document.getElementById("nav-bar");
 const navbar_links = document.querySelectorAll("#nav-bar a");
 const featured_section = document.querySelectorAll("#featured-section");
+const popular_text = document.querySelectorAll(".popular-item h2, .popular-item p");
+
 const footer_section = document.getElementsByTagName("footer");
 
 const body = document.body;
@@ -12,6 +14,7 @@ const order_button = document.getElementById("order-button");
 let toggle = false;
 
 darkMode_button.addEventListener("click", function () {
+
   body.classList.toggle("dark-mode");
 
   toggle = !toggle;
@@ -19,13 +22,13 @@ darkMode_button.addEventListener("click", function () {
   if (toggle) {
     // code for when in DARK Mode
 
-    //   go back to light mode
+    //  Theme Mode button
     darkMode_button.style.backgroundColor = "white";
     darkMode_button.style.color = "black";
     darkMode_button.textContent = "Light Mode";
-
     darkMode_button.style.boxShadow = "rgba(255, 255, 255, 0.9) 0px 2px 10px";
 
+    // Nav bar
     navbar.style.backgroundColor = "#333333";
     // code to change the navbar links, using a "for of" loop
     for (const element of navbar_links) {
@@ -35,7 +38,13 @@ darkMode_button.addEventListener("click", function () {
     for (const element of featured_section) {
       element.style.color = "black";
     }
-     // code to change the text in "featured section", using a "for of" loop
+    // cod to change the text in the popular menu items section
+    for (const element of popular_text) {
+      element.style.color = "black";
+    }
+
+
+     // code to change the text in "footer section", using a "for of" loop
      for (const element of footer_section) {
         element.style.color = "black";
       }
