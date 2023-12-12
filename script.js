0d3b79d62a16060ca47501b22ba3c6fd1377eaae
mark-elias <mark7elias@gmail.com>
2023-12-11 23:46:56 -0800

    working links to other pages. navbar fully working

---
+++ script.js
@@ -0,0 +1,12 @@
+const darkMode_button = document.querySelector(".fa-circle-half-stroke");
+
+let toggle = false;
+darkMode_button.addEventListener("onClick", function(){
+    toggle = !toggle;
+
+    
+
+})
+
+
+
