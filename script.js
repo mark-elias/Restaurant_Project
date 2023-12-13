e56f7f99e2bf4c0ce26fb7b9dd10b3724cb0749c
mark-elias <mark7elias@gmail.com>
2023-12-13 12:47:13 -0800

    updated DarkMode, is now persistent

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+// ---------- Selecting Elements ----------
 // get DarkMode button
 const darkMode_button = document.querySelector(".fa-circle-half-stroke");
-
 // navbar
 const nav = document.querySelector("nav");
 const logoSection = document.querySelector(".logo-section a");
@@ -9,35 +9,41 @@ const navLinks = document.querySelectorAll(".nav-links li a, .nav-links li i");
 const main = document.querySelector("main");
 const body = document.body;
 
+// ----- setting Persistent Variable and toggle -----
+localStorage.getItem("darkMode", false);
 let toggle = false;
+
+// Function to apply dark mode styles
+function applyTheme() {
+  // Retrieve the user's preference from localStorage
+
+  toggle = localStorage.getItem("darkMode") === "true";
+
+  // navbar
+  nav.style.backgroundColor = toggle ? "#202020" : "white";
+  logoSection.style.color = toggle ? "white" : "black";
+  logoSection.style.border = toggle ? "1px solid white" : "1px solid black";
+  for (link of navLinks) {
+    link.style.color = toggle ? "white" : "black";
+  }
+  // body and main
+  main.style.backgroundColor = toggle ? "#202020" : "white";
+  main.style.color = toggle ? "white" : "black";
+  body.style.backgroundColor = toggle ? "#202020" : "white";
+}
+
+// Apply dark mode styles when the page loads
+applyTheme();
+
+// Dark mode button click event
 darkMode_button.addEventListener("click", function () {
+  // Toggle the dark mode preference
+  toggle = localStorage.getItem("darkMode") === "true";
   toggle = !toggle;
 
-  console.log("clicked");
+  // Save the updated preference to localStorage
+  localStorage.setItem("darkMode", toggle);
 
-  if (toggle) {
-    //navbar
-    nav.style.backgroundColor = "#202020";
-    logoSection.style.color = "white";
-    logoSection.style.border = "1px solid white";
-    for (link of navLinks) {
-      link.style.color = "white";
-    }
-    //body and main
-    main.style.backgroundColor = "#202020";
-    main.style.color = "white";
-    body.style.backgroundColor = "#202020";
-  } else {
-    //navbar
-    nav.style.backgroundColor = "white";
-    logoSection.style.color = "black";
-    logoSection.style.border = "1px solid black";
-    for (link of navLinks) {
-      link.style.color = "black";
-    }
-    //body and main
-    main.style.backgroundColor = "white";
-    main.style.color = "black";
-    body.style.backgroundColor = "white";
-  }
+  // Apply dark mode styles
+  applyTheme();
 });
